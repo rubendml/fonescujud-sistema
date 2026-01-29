954ea783f487db55815a74ae2541294542e11975
Tu Nombre <tu@email.com>
2026-01-29 15:11:50 -0500

    Aceptar variable SUPABASE_SERVICE_ROL_KEY

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,7 +6,9 @@ export const config = {
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    serviceRoleKey:
+      process.env.SUPABASE_SERVICE_ROLE_KEY ||
+      process.env.SUPABASE_SERVICE_ROL_KEY,
   },
   server: {
     port: process.env.PORT || 3000,
